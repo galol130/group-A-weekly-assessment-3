c88e683c15c1dc33c0b55a11c8ffd1b4b595be4b
dani <danijuancar@gmail.com>
2021-01-30 12:02:53 +0100

    Added SalesRep class: not edited, just created.

---
+++ src/main/java/com/ironhack/classes/SalesRep.java
@@ -0,0 +1,28 @@
+package com.ironhack.classes;
+
+public class SalesRep {
+    //This class has only been created, it will be changed when we start working on it.
+    private Integer id;
+    private String name;
+
+    public SalesRep(Integer id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
