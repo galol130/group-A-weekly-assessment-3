17e5002b1682be5bc65e4ab27e64e56b1ff51dd3
dani <danijuancar@gmail.com>
2021-02-04 21:56:40 +0100

    few queries

--- src/main/java/com/ironhack/repository/OpportunityRepository.java
+++ src/main/java/com/ironhack/repository/OpportunityRepository.java
@@ -1,12 +1,23 @@
 package com.ironhack.repository;
 
+
 import com.ironhack.model.Opportunity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.persistence.NamedQuery;
 import java.util.List;
 
 @Repository
 public interface OpportunityRepository extends JpaRepository<Opportunity, Integer> {
     public List<Opportunity> findByAccountId (Integer id);
+
+    @Query("SELECT MAX(quantity) FROM Opportunity o WHERE o.status = com.ironhack.enums.Status.CLOSED_WON")
+    public Integer findMaxProductsOrdered();
+
+    @Query("SELECT MIN(quantity) FROM Opportunity o WHERE o.status = com.ironhack.enums.Status.CLOSED_WON")
+    public Integer findMinProductsOrdered();
+
+
 }
